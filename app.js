947e2dd3e558406e2ab698949ed24af8348349f7
Hery Vandoro <vandorohery99@gmail.com>
2017-05-12 22:45:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,46 +12,44 @@ app.controller("ocrController", function($scope, $http){
 
   	$scope.getResult = function(){
   		$scope.vision_api_json = {
-	      "requests":[
-	        {
-	          "image":{
-	          	"content" : $scope.imgBase64
-	          },
-	          "features":[
-	            {
-	              "type": "TEXT_DETECTION",
-	              "maxResults": 1
-	            }
-	          ]
-	        }
-	      ]
-	    };
+		      "requests":[
+			{
+			  "image":{
+				"content" : $scope.imgBase64
+			  },
+			  "features":[
+			    {
+			      "type": "TEXT_DETECTION",
+			      "maxResults": 1
+			    }
+			  ]
+			}
+		      ]
+		};
 
   		$http({
-        	url : "https://vision.googleapis.com/v1/images:annotate?key="+apiKey,
-        	method : "POST",
-        	data : $scope.vision_api_json
-        }).then(function(response){
-        	$scope.result = response.data.responses[0].fullTextAnnotation.text;
-        });
+			url : "https://vision.googleapis.com/v1/images:annotate?key="+apiKey,
+			method : "POST",
+			data : $scope.vision_api_json
+		}).then(function(response){
+			$scope.result = response.data.responses[0].fullTextAnnotation.text;
+		});
   	}
 
 	$scope.uploadPicture = function (ele) {
-		
 	  	img.src = URL.createObjectURL(ele.files[0]); 
-	    img.onload = function() {
-	    	c.height = img.height;
-	    	c.width = img.width;
+	    	img.onload = function() {
+			c.height = img.height;
+			c.width = img.width;
 			//draw selected image to canvas
-	        ctx.drawImage(img, 0, 0);
+			ctx.drawImage(img, 0, 0);
 
-	        //get base64 and set to result section
-	        var result = c.toDataURL();
-	        $scope.imgBase64 = result.replace('data:image/png;base64,','', result);
-	        alert($scope.imgBase64);
+			//get base64 and set to result section
+			var result = c.toDataURL();
+			$scope.imgBase64 = result.replace('data:image/png;base64,','', result);
+			//alert($scope.imgBase64);
 			$scope.$apply();
 			$scope.getResult();
-	    }
+		}
 	}
-
 })
